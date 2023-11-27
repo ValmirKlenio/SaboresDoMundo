1ea59bd65e727fbd2978a49c4cfad33018fa9b82
samyrafariass <104604760+samyrafariass@users.noreply.github.com>
2023-11-27 18:36:15 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+# bibliotecas/imports
+import csv
+
+# listas/dicionarios
+estoque = []
+funcionarios = []
+area_acesso = []
+cardapios = []
+fornecedores = []
+
+# menu
+def menu():
+  print('''Escolha uma opção:
+  [ 1 ] - Controle de Estoque
+  [ 2 ] - Cadastro de Perfis de Acesso
+  [ 3 ] - Gestão de Funcionários
+  [ 4 ] - Monitoramento da Produção
+  [ 5 ] - Pedidos a fornecedores
+  [ 6 ] - Relatórios Gerenciais
+  [ 7 ] - Sair/Encerrar''')
+
+# modulo 1 - Samyra
+
+# modulo 2 - Valmir
+
+# modulo 3 - Samyra
+
+# modulo 4 - Valmir
+
+# modulo 5 - Vitória
+
+# modulo 6 - Vitória/Pedro
+
+# chamada das funções (MAIN)
